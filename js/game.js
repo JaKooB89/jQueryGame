06cb62afd1e774fbf5d6b388046febf6f7e1a365
JaKooB89 <30555035+JaKooB89@users.noreply.github.com>
2018-01-16 21:58:58 +0000

    JS cleanup

--- js/game.js
+++ js/game.js
@@ -2,77 +2,77 @@
  * @author Jakub Czaja <jakoob89@gmail.com>
  * Copyright (c) 2018 Jakub Czaja
  **/
- 
-// Preloads Game Assets
-$.preload([
-'images/bg1.jpg',
-'images/bg2.jpg',
-'images/bg3.jpg',
-'images/bg4.jpg',
-'images/bg5.jpg',
-'images/bg6.jpg',
-'images/bg7.jpg',
-'images/bg8.jpg',
-'audio/click.mp3',
-'audio/click2.mp3',
-'audio/shutter.mp3'
-]);
 
-//Initial Variables
-var pointsTracker = 0;
-var count = 10;
-var sound = true;
+// Initial Variables
+var pointsTracker = 0
+var count = 10
+var sound = true
 
-$(function() {
+$(function () {
+  // Preloads Game Assets
+  $.preload([
+    'images/bg1.jpg',
+    'images/bg2.jpg',
+    'images/bg3.jpg',
+    'images/bg4.jpg',
+    'images/bg5.jpg',
+    'images/bg6.jpg',
+    'images/bg7.jpg',
+    'images/bg8.jpg',
+    'audio/click.mp3',
+    'audio/click2.mp3',
+    'audio/shutter.mp3'
+  ])
+
   // Difficulty Level Choice
-  $('.btnDif').click(function() {
-    var btnId = $(this).attr("data-dif-id");
-    count = btnId;
-    $(".btnDif").addClass("disabled");
-    $(this).removeClass("disabled");
-    $("#btnStart").removeAttr("disabled");
-  });
+  $('.btnDif').click(function () {
+    var btnId = $(this).attr('data-dif-id')
+    count = btnId
+    $('.btnDif').addClass('disabled')
+    $(this).removeClass('disabled')
+    $('#btnStart').removeAttr('disabled')
+  })
 
   // Button Sound
-  $('#gameContainer button').click(function() {
+  $('#gameContainer button').click(function () {
     if (sound === true) {
-      $.playSound("audio/click.mp3");
+      $.playSound('audio/click.mp3')
     };
-  });
-  $('.btnDif').click(function() {
+  })
+  $('.btnDif').click(function () {
     if (sound === true) {
-      $.stopSound();
-      $.playSound("audio/click2.mp3");
+      $.stopSound()
+      $.playSound('audio/click2.mp3')
     };
-  });
+  })
 
   // Mute Function
-  $('#btnMute').click(function() {
-    $("#btnMute svg").toggleClass("fa-volume-off");
-    $("#btnMute svg").toggleClass("fa-volume-up");
+  $('#btnMute').click(function () {
+    $('#btnMute svg').toggleClass('fa-volume-off')
+    $('#btnMute svg').toggleClass('fa-volume-up')
     if (sound === true) {
-      sound = false;
+      sound = false
     } else {
-      sound = true;
+      sound = true
     };
-  });
+  })
 
   // Screen Changer
-  $(".scrChange").on("click", function() {
-    var scrId = $(this).attr("data-scr-id");
-    $(".screen").fadeOut(300, function() {
-      setTimeout(function() {
-        $("#screen"+scrId).fadeIn();
-      }, 301);
-    });
-  });
+  $('.scrChange').on('click', function () {
+    var scrId = $(this).attr('data-scr-id')
+    $('.screen').fadeOut(300, function () {
+      setTimeout(function () {
+        $('#screen' + scrId).fadeIn()
+      }, 301)
+    })
+  })
 
   // Background Changer
-  $(".backThumb").on("click", function() {
-    var bgId = $(this).attr("data-bg-id");
-    $("#gameContainer").css("background-image", "url(images/bg"+bgId+".jpg)");
+  $('.backThumb').on('click', function () {
+    var bgId = $(this).attr('data-bg-id')
+    $('#gameContainer').css('background-image', 'url(images/bg' + bgId + '.jpg)')
     if (sound === true) {
-      $.playSound("audio/shutter.mp3");
+      $.playSound('audio/shutter.mp3')
     };
-  });
-});
+  })
+})
